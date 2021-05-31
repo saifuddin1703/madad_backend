b25c62fec7bdec5d023b2a66b50950495253e1d8
Saifuddin225 <saifuddinahmed50@gmail.com>
2021-05-31 13:43:27 +0530

    new

--- src/myapi.js
+++ src/myapi.js
@@ -3,15 +3,16 @@ const userrout= require("./routers/user")
 const auth= require("./routers/authentication")
 const app= express();
 const http= require('http')
-const server= http.createServer(app)
-const io= require('socket.io')(server)
-const port=process.env.PORT ||8000;
 
+const port=process.env.PORT ||8000;
+const server= app.listen(port)
+
+const io= require('socket.io')(server)
 require("./db/db")
 
-server.listen(port,()=>{
-    console.log("listening on port : "+port);
-});
+// server.listen(port,()=>{
+//     console.log("listening on port : "+port);
+// });
 app.use(express.json())
 app.use("/user",userrout);
 app.use('/uploads', express.static('./uploads'));
